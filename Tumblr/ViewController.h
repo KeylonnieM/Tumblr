399c2eabc3687e8ce099ba8483eccb1ef23535c0
Keylonnie Miller <keylonniem@fb.com>
2018-06-27 13:00:19 -0700

    Initial Commit

---
+++ Tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by Keylonnie Miller on 6/27/18.
+//  Copyright © 2018 Keylonnie Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tumblr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tumblr
+//
+//  Created by Keylonnie Miller on 6/27/18.
+//  Copyright © 2018 Keylonnie Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
